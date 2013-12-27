7c5f5fd5e58fe336e16ff9c8be14a87797104dd2
Michael Epstein <michaelepstein@ITs-MacBook-Pro.local>
2013-12-27 13:06:00 -0800

    add support for float,double and short just for the sake of completeness

--- src/test/java/com/mylife/hbase/mapper/model/TestModel.java
+++ src/test/java/com/mylife/hbase/mapper/model/TestModel.java
@@ -37,26 +37,54 @@ import com.mylife.hbase.mapper.annotation.HBaseRowKey;
 @HBasePersistance(tableName = "TEST_MODEL", defaultColumnFamilyName = "STUFF")
 public class TestModel {
 
-    @HBaseField(columnFamilyName="OTHER_STUFF")
+    @HBaseField
+    private Integer integerField;
+
+    @HBaseField
+    private Short shortField;
+
+    @HBaseField
+    private Double doubleField;
+
+    @HBaseField
+    private Float floatField;
+
+    @HBaseField(columnFamilyName = "OTHER_STUFF")
     private Long longField;
 
     @HBaseField
     private String stringField;
 
-    @HBaseField(columnFamilyName="MORE_STUFF")
+    @HBaseField(columnFamilyName = "MORE_STUFF")
     private Boolean booleanField;
 
-    @HBaseField(columnFamilyName="OTHER_STUFF")
+    @HBaseField(columnFamilyName = "OTHER_STUFF")
     private byte[] byteArrayField;
 
     @HBaseField
     private ContentType contentTypeField;
-    
+
     @HBaseMapField
     private Map<Long, Object> badMap;
-    
+
     @HBaseMapField
     private List<Long> notAMap;
+
+    public Integer getIntegerField() {
+        return integerField;
+    }
+
+    public Short getShortField() {
+        return shortField;
+    }
+
+    public Double getDoubleField() {
+        return doubleField;
+    }
+
+    public Float getFloatField() {
+        return floatField;
+    }
 
     public Long getLongField() {
         return longField;
@@ -78,9 +106,21 @@ public class TestModel {
         return contentTypeField;
     }
 
-    public TestModel(Long longField, String stringField, Boolean booleanField, byte[] byteArrayField,
-            ContentType contentTypeField) {
+    public Map<Long, Object> getBadMap() {
+        return badMap;
+    }
+
+    public List<Long> getNotAMap() {
+        return notAMap;
+    }
+
+    public TestModel(Integer integerField, Short shortField, Double doubleField, Float floatField, Long longField,
+            String stringField, Boolean booleanField, byte[] byteArrayField, ContentType contentTypeField) {
         super();
+        this.integerField = integerField;
+        this.shortField = shortField;
+        this.doubleField = doubleField;
+        this.floatField = floatField;
         this.longField = longField;
         this.stringField = stringField;
         this.booleanField = booleanField;
@@ -93,10 +133,16 @@ public class TestModel {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((badMap == null) ? 0 : badMap.hashCode());
         result = prime * result + ((booleanField == null) ? 0 : booleanField.hashCode());
         result = prime * result + Arrays.hashCode(byteArrayField);
         result = prime * result + ((contentTypeField == null) ? 0 : contentTypeField.hashCode());
+        result = prime * result + ((doubleField == null) ? 0 : doubleField.hashCode());
+        result = prime * result + ((floatField == null) ? 0 : floatField.hashCode());
+        result = prime * result + ((integerField == null) ? 0 : integerField.hashCode());
         result = prime * result + ((longField == null) ? 0 : longField.hashCode());
+        result = prime * result + ((notAMap == null) ? 0 : notAMap.hashCode());
+        result = prime * result + ((shortField == null) ? 0 : shortField.hashCode());
         result = prime * result + ((stringField == null) ? 0 : stringField.hashCode());
         return result;
     }
@@ -110,6 +156,11 @@ public class TestModel {
         if (getClass() != obj.getClass())
             return false;
         TestModel other = (TestModel) obj;
+        if (badMap == null) {
+            if (other.badMap != null)
+                return false;
+        } else if (!badMap.equals(other.badMap))
+            return false;
         if (booleanField == null) {
             if (other.booleanField != null)
                 return false;
@@ -122,10 +173,35 @@ public class TestModel {
                 return false;
         } else if (!contentTypeField.toString().equals(other.contentTypeField.toString()))
             return false;
+        if (doubleField == null) {
+            if (other.doubleField != null)
+                return false;
+        } else if (!doubleField.equals(other.doubleField))
+            return false;
+        if (floatField == null) {
+            if (other.floatField != null)
+                return false;
+        } else if (!floatField.equals(other.floatField))
+            return false;
+        if (integerField == null) {
+            if (other.integerField != null)
+                return false;
+        } else if (!integerField.equals(other.integerField))
+            return false;
         if (longField == null) {
             if (other.longField != null)
                 return false;
         } else if (!longField.equals(other.longField))
+            return false;
+        if (notAMap == null) {
+            if (other.notAMap != null)
+                return false;
+        } else if (!notAMap.equals(other.notAMap))
+            return false;
+        if (shortField == null) {
+            if (other.shortField != null)
+                return false;
+        } else if (!shortField.equals(other.shortField))
             return false;
         if (stringField == null) {
             if (other.stringField != null)
@@ -137,12 +213,11 @@ public class TestModel {
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("TestModel [longField=").append(longField).append(", stringField=").append(stringField)
-                .append(", booleanField=").append(booleanField).append(", byteArrayField=")
-                .append(Arrays.toString(byteArrayField)).append(", contentTypeField=").append(contentTypeField)
-                .append(", badMap=").append(badMap).append(", notAMap=").append(notAMap).append("]");
-        return builder.toString();
+        return "TestModel [integerField=" + integerField + ", shortField=" + shortField + ", doubleField="
+                + doubleField + ", floatField=" + floatField + ", longField=" + longField + ", stringField="
+                + stringField + ", booleanField=" + booleanField + ", byteArrayField="
+                + Arrays.toString(byteArrayField) + ", contentTypeField=" + contentTypeField + ", badMap=" + badMap
+                + ", notAMap=" + notAMap + "]";
     }
-    
+
 }
